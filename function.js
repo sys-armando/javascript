111b619e85a3b21f9eb822c1969eb3632a0e9c39
sys-armando <manusiwapatrick@gmail.com>
2022-04-14 22:46:05 +0700

    function

---
+++ function.js
@@ -0,0 +1,15 @@
+function jalanSatu(){
+    console.log("jalan satu")
+}
+
+const jalanDua = function(){
+    console.log("jalan dua")
+}
+
+const jalanTiga = ()=>{
+    console.log("jalan tiga")
+}
+
+jalanSatu()
+jalanDua()
+jalanTiga()
