02c53faf9016b3392b777b1505c77db5f2c116ad
sys-armando <manusiwapatrick@gmail.com>
2022-04-14 22:21:15 +0700

    boolean.js

---
+++ boolean.js
@@ -0,0 +1,12 @@
+var tinggi;
+var cm = 170;
+
+if(cm > 160){
+    tinggi = true;
+    console.log("Tinggi Melebihi Standart")
+}else{
+    tinggi = false;
+    console.log("Tinggi Kurang")
+}
+
+console.log(tinggi)
