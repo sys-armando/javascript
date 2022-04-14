43fcab3a6919caba00edfff48ba23a0b48ddada1
sys-armando <manusiwapatrick@gmail.com>
2022-04-14 22:31:24 +0700

    array.js

---
+++ array.js
@@ -0,0 +1,2 @@
+var angka = [3,4,5,6,7];
+console.log(angka[1])
---
+++ null.js
@@ -0,0 +1,7 @@
+var kosong = "ada";
+kosong = null
+var undefined;
+console.log(kosong)
+console.log(undefined)
+undefined = "coba isi undefined";
+console.log(undefined)
