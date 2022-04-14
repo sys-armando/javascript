409ecba314403c68010b0cbdf24c49297682364e
sys-armando <manusiwapatrick@gmail.com>
2022-04-14 21:50:00 +0700

    tugas1.js

---
+++ index.js
@@ -0,0 +1,6 @@
+var message = "Hello Coding";
+let msg1 = "Haloo";
+const msg2 = "Coding";
+console.log(message)
+console.log(msg1+" "+msg2)
+console.log(msg2)
---
+++ tugas1.js
@@ -0,0 +1,7 @@
+var namaDepan = "Patrick";
+let namaBelakang = "Armando";
+const umur = 34;
+
+console.log(namaDepan)
+console.log(namaBelakang)
+console.log(umur)
