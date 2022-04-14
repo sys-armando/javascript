2071a96c8289f9960a45db2988ca3e40f52ec96e
sys-armando <manusiwapatrick@gmail.com>
2022-04-14 22:36:42 +0700

    nulll.js

---
+++ tugas2.js
@@ -0,0 +1,13 @@
+var number = 1987;
+var string = "Patrick Armando";
+var boolean = true;
+var undefined = null;
+var nulll;
+var array = [01,10,1987,"Patrick","Armando"];
+
+console.log(number)
+console.log(string)
+console.log(boolean)
+console.log(undefined)
+console.log(nulll)
+console.log(array)
